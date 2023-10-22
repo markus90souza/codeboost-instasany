3361526e01aaf47ccbbcf770e43b88ce118bbeec
rockske8er <rockske8er@gmail.com>
2023-10-22 11:33:32 -0300

    chore: add assets for project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,10 @@ import type { Metadata } from 'next'
 import { Epilogue } from 'next/font/google'
 import { ReactNode, FC } from 'react'
 
-const epilogue = Epilogue({ subsets: ['latin'] })
+const epilogue = Epilogue({
+  subsets: ['latin'],
+  weight: ['500', '600', '700'],
+})
 
 export const metadata: Metadata = {
   title: 'Instasany',
